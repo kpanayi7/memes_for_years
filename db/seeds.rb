7452172f696f171d3f1583501e8125d38d281c65
Kyri Panayi <kpanayi7@gmail.com>
2022-02-12 16:05:52 +0000

    user changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,40 +17,40 @@ file6 = URI.open('https://res.cloudinary.com/dza6mn30y/image/upload/v1644368306/
 MemeCreatorListing.destroy_all
 User.destroy_all
 
-User.create!(name:"Dave", email:"dave@pushflo.com", password:"1234567")
+user_1 = User.create!(name:"Dave", email:"dave@pushflo.com", password:"1234567")
 puts "created user"
-User.create!(name:"Kyri", email:"kyri@pufhflo.com", password:"7654321")
+user_2 = User.create!(name:"Kyri", email:"kyri@pufhflo.com", password:"7654321")
 puts "created user"
-User.create!(name:"Nelson", email:"nelson@pushflo.com", password:"1234567")
+user_3 = User.create!(name:"Nelson", email:"nelson@pushflo.com", password:"1234567")
 puts "created user"
-User.create!(name:"Rahul", email:"rahul@pushflo.com", password:"1234567")
+user_4 = User.create!(name:"Rahul", email:"rahul@pushflo.com", password:"1234567")
 puts "created user"
-User.create!(name:"Meehir", email:"meehir@pufhflo.com", password:"1234567")
+user_5 = User.create!(name:"Meehir", email:"meehir@pufhflo.com", password:"1234567")
 puts "created user"
-User.create!(name:"Zak", email:"zak@pushflo.com", password:"1234567")
+user_6 = User.create!(name:"Zak", email:"zak@pushflo.com", password:"1234567")
 puts "created user"
 
-eggmeme=MemeCreatorListing.new(creator_name:"Eggmeister", speciality:"Egg Meme Specialist", user_id:1)
+eggmeme=MemeCreatorListing.new(creator_name:"Eggmeister", speciality:"Egg Meme Specialist", user_id:user_1.id)
 eggmeme.photo.attach(io: file1, filename: "egg.png", content_type: 'image/jpg')
 eggmeme.save!
 
-catmeme=MemeCreatorListing.new(creator_name:"Meeyaaaw", speciality:"Cat Meme Specialist", user_id:2)
+catmeme=MemeCreatorListing.new(creator_name:"Meeyaaaw", speciality:"Cat Meme Specialist", user_id:user_2.id)
 catmeme.photo.attach(io: file2, filename: "cat.png", content_type: 'image/jpg')
 catmeme.save!
 
-borismeme=MemeCreatorListing.new(creator_name:"Bozza", speciality:"Boris Johnson Meme Specialist", user_id:3)
+borismeme=MemeCreatorListing.new(creator_name:"Bozza", speciality:"Boris Johnson Meme Specialist", user_id:user_3.id)
 borismeme.photo.attach(io: file3, filename: "boris.png", content_type: 'image/jpg')
 borismeme.save!
 
-djmeme=MemeCreatorListing.new(creator_name:"Deejay", speciality:"DJ Meme Specialist", user_id:4)
+djmeme=MemeCreatorListing.new(creator_name:"Deejay", speciality:"DJ Meme Specialist", user_id:user_4.id)
 djmeme.photo.attach(io: file4, filename: "dj.png", content_type: 'image/jpg')
 djmeme.save!
 
-dogmeme=MemeCreatorListing.new(creator_name:"Hugh Let the Dogs Out", speciality:"Dog Meme Specialist", user_id:5)
+dogmeme=MemeCreatorListing.new(creator_name:"Hugh Let the Dogs Out", speciality:"Dog Meme Specialist", user_id:user_5.id)
 dogmeme.photo.attach(io: file5, filename: "dog.png", content_type: 'image/jpg')
 dogmeme.save!
 
-codingmeme=MemeCreatorListing.new(creator_name:"Query Selecta Ta Ta", speciality:"Coding Meme Specialist", user_id:6)
+codingmeme=MemeCreatorListing.new(creator_name:"Query Selecta Ta Ta", speciality:"Coding Meme Specialist", user_id:user_6.id)
 codingmeme.photo.attach(io: file6, filename: "coding.png", content_type: 'image/jpg')
 codingmeme.save!
 
